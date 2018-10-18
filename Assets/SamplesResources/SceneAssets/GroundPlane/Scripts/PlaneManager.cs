16d80ac3f29479cea85c33b282bca02ee494a1da
Christian Koernig <christian.koernig@esa.int>
2018-10-18 15:35:35 +0200

    Added magellan_ex spacecraft as MidAir object

--- Assets/SamplesResources/SceneAssets/GroundPlane/Scripts/PlaneManager.cs
+++ Assets/SamplesResources/SceneAssets/GroundPlane/Scripts/PlaneManager.cs
@@ -26,6 +26,7 @@ public class PlaneManager : MonoBehaviour
     public GameObject m_PlaneAugmentation;
     public GameObject m_MidAirAugmentation;
     public GameObject m_PlacementAugmentation;
+    public GameObject m_MidAirAugmentation2;
     public static bool GroundPlaneHitReceived, AstronautIsPlaced;
     public static PlaneMode planeMode = PlaneMode.PLACEMENT;
 
@@ -51,7 +52,7 @@ public class PlaneManager : MonoBehaviour
     TouchHandler m_TouchHandler;
     ProductPlacement m_ProductPlacement;
     GroundPlaneUI m_GroundPlaneUI;
-    AnchorBehaviour m_PlaneAnchor, m_MidAirAnchor, m_PlacementAnchor;
+    AnchorBehaviour m_PlaneAnchor, m_MidAirAnchor, m_PlacementAnchor, m_MidAirAnchor2;
     int AutomaticHitTestFrameCount;
     int m_AnchorCounter;
     bool uiHasBeenInitialized;
@@ -77,10 +78,12 @@ public class PlaneManager : MonoBehaviour
         m_PlaneAnchor = m_PlaneAugmentation.GetComponentInParent<AnchorBehaviour>();
         m_MidAirAnchor = m_MidAirAugmentation.GetComponentInParent<AnchorBehaviour>();
         m_PlacementAnchor = m_PlacementAugmentation.GetComponentInParent<AnchorBehaviour>();
+        m_MidAirAnchor2 = m_MidAirAugmentation2.GetComponentInParent<AnchorBehaviour>();
 
         UtilityHelper.EnableRendererColliderCanvas(m_PlaneAugmentation, false);
         UtilityHelper.EnableRendererColliderCanvas(m_MidAirAugmentation, false);
         UtilityHelper.EnableRendererColliderCanvas(m_PlacementAugmentation, false);
+        UtilityHelper.EnableRendererColliderCanvas(m_MidAirAugmentation2, false);
     }
 
     void Update()
@@ -196,6 +199,13 @@ public class PlaneManager : MonoBehaviour
 
             m_MidAirAugmentation.transform.localPosition = Vector3.zero;
             UtilityHelper.RotateTowardCamera(m_MidAirAugmentation);
+
+            m_ContentPositioningBehaviour.AnchorStage = m_MidAirAnchor2;
+            m_ContentPositioningBehaviour.PositionContentAtMidAirAnchor(midAirTransform);
+            UtilityHelper.EnableRendererColliderCanvas(m_MidAirAugmentation2, true);
+
+            m_MidAirAugmentation2.transform.localPosition = Vector3.zero;
+            UtilityHelper.RotateTowardCamera(m_MidAirAugmentation2);
         }
     }
 
@@ -253,6 +263,10 @@ public class PlaneManager : MonoBehaviour
         m_MidAirAugmentation.transform.localEulerAngles = Vector3.zero;
         UtilityHelper.EnableRendererColliderCanvas(m_MidAirAugmentation, false);
 
+        m_MidAirAugmentation2.transform.position = Vector3.zero;
+        m_MidAirAugmentation2.transform.localEulerAngles = Vector3.zero;
+        UtilityHelper.EnableRendererColliderCanvas(m_MidAirAugmentation2, false);
+
         m_ProductPlacement.Reset();
         UtilityHelper.EnableRendererColliderCanvas(m_PlacementAugmentation, false);
 
@@ -286,6 +300,7 @@ public class PlaneManager : MonoBehaviour
         m_PlaneAnchor.UnConfigureAnchor();
         m_MidAirAnchor.UnConfigureAnchor();
         m_PlacementAnchor.UnConfigureAnchor();
+        m_MidAirAnchor2.UnConfigureAnchor();
         AnchorExists = DoAnchorsExist();
     }
 
