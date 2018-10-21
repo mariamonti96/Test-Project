8c110f421f469e43c8fcaa8662fac0689d4b0354
Maria Chiara Monti <mariamonti96@gmail.com>
2018-10-21 19:09:53 +0200

    Added GroundPlaneTest scene with working reticle and spacecraft placement

--- Assets/Scripts/GroundPlaneTestUI.cs
+++ Assets/Scripts/GroundPlaneTestUI.cs
@@ -10,26 +10,26 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class MidAirUI : MonoBehaviour
+public class GroundPlaneTestUI : MonoBehaviour
 {
     #region PUBLIC_MEMBERS
     [Header("UI Elements")]
-    public Text m_Title;
+    //public Text m_Title;
     public Text m_TrackerStatus;
-    public Text m_Instructions;
+    //public Text m_Instructions;
     public CanvasGroup m_ScreenReticle;
 
-    [Header("UI Buttons")]
-    public Button m_ResetButton;
-    public Toggle m_PlacementToggle, m_GroundToggle, m_MidAirToggle, m_MidAirToggle2;
+    //[Header("UI Buttons")]
+    //public Button m_ResetButton;
+    //public Toggle m_PlacementToggle, m_GroundToggle, m_MidAirToggle, m_MidAirToggle2;
     #endregion // PUBLIC_MEMBERS
 
 
     #region PRIVATE_MEMBERS
-    const string TITLE_PLACEMENT = "Product Placement";
-    const string TITLE_GROUNDPLANE = "Ground Plane";
-    const string TITLE_MIDAIR = "Mid-Air";
-    const string TITLE_MIDAIR2 = "Mid-Air Spacecraft";
+    //const string TITLE_PLACEMENT = "Product Placement";
+    //const string TITLE_GROUNDPLANE = "Ground Plane";
+    //const string TITLE_MIDAIR = "Mid-Air";
+    //const string TITLE_MIDAIR2 = "Mid-Air Spacecraft";
 
     GraphicRaycaster m_GraphicRayCaster;
     PointerEventData m_PointerEventData;
@@ -45,12 +45,12 @@ public class MidAirUI : MonoBehaviour
     #region MONOBEHAVIOUR_METHODS
     void Start()
     {
-        m_ResetButton.interactable = m_MidAirToggle.interactable =
-        m_GroundToggle.interactable = m_PlacementToggle.interactable =
-                m_MidAirToggle2.interactable = false;
+        //m_ResetButton.interactable = m_MidAirToggle.interactable =
+        //m_GroundToggle.interactable = m_PlacementToggle.interactable = 
+        //        m_MidAirToggle2.interactable = false;
 
 
-        m_Title.text = TITLE_PLACEMENT;
+        //m_Title.text = TITLE_PLACEMENT;
         m_TrackerStatus.text = "";
         m_TrackerStatusImage = m_TrackerStatus.GetComponentInParent<Image>();
 
@@ -83,47 +83,48 @@ public class MidAirUI : MonoBehaviour
             // Hide the onscreen reticle when we get a hit test
             m_ScreenReticle.alpha = 0;
 
-            m_Instructions.transform.parent.gameObject.SetActive(true);
-            m_Instructions.enabled = true;
+            //m_Instructions.transform.parent.gameObject.SetActive(true);
+            //m_Instructions.enabled = true;
 
-            if (PlaneManager.planeMode == PlaneManager.PlaneMode.GROUND)
-            {
-                m_Instructions.text = "Tap to place Astronaut";
-            }
-            else if (PlaneManager.planeMode == PlaneManager.PlaneMode.PLACEMENT)
-            {
-                m_Instructions.text = (m_ProductPlacement.IsPlaced) ?
-                    "• Touch and drag to move Chair" +
-                    "\n• Two fingers to rotate" +
-                    ((m_TouchHandler.enablePinchScaling) ? " or pinch to scale" : "") +
-                    "\n• Double-tap to reset Anchor location"
-                    :
-                    "Tap to place Chair";
-            }
+            //if (PlaneManager.planeMode == PlaneManager.PlaneMode.GROUND)
+            //{
+            //    m_Instructions.text = "Tap to place Astronaut";
+            //}
+            //else if (PlaneManager.planeMode == PlaneManager.PlaneMode.PLACEMENT)
+            //{
+            //    m_Instructions.text = (m_ProductPlacement.IsPlaced) ?
+            //        "• Touch and drag to move Chair" +
+            //        "\n• Two fingers to rotate" +
+            //        ((m_TouchHandler.enablePinchScaling) ? " or pinch to scale" : "") +
+            //        "\n• Double-tap to reset Anchor location"
+            //        :
+            //        "Tap to place Chair";
+            //}
         }
         else
         {
             // No automatic hit test, so set alpha based on which plane mode is active
-            m_ScreenReticle.alpha =
-                (PlaneManager.planeMode == PlaneManager.PlaneMode.GROUND ||
-                PlaneManager.planeMode == PlaneManager.PlaneMode.PLACEMENT) ? 1 : 0;
+            //m_ScreenReticle.alpha =
+            //(PlaneManager.planeMode == PlaneManager.PlaneMode.GROUND ||
+            //PlaneManager.planeMode == PlaneManager.PlaneMode.PLACEMENT) ? 1 : 0;
 
-            m_Instructions.transform.parent.gameObject.SetActive(true);
-            m_Instructions.enabled = true;
+            m_ScreenReticle.alpha = 1;
 
-            if (PlaneManager.planeMode == PlaneManager.PlaneMode.GROUND ||
-                PlaneManager.planeMode == PlaneManager.PlaneMode.PLACEMENT)
-            {
-                m_Instructions.text = "Point device towards ground";
-            }
-            else if (PlaneManager.planeMode == PlaneManager.PlaneMode.MIDAIR)
-            {
-                m_Instructions.text = "Tap to place Drone";
-            }
-            else if (PlaneManager.planeMode == PlaneManager.PlaneMode.MIDAIR2)
-            {
-                m_Instructions.text = "Tap to place Spacecraft";
-            }
+            //m_Instructions.transform.parent.gameObject.SetActive(true);
+            //m_Instructions.enabled = true;
+
+            //if (PlaneManager.planeMode == PlaneManager.PlaneMode.GROUND ||
+            //    PlaneManager.planeMode == PlaneManager.PlaneMode.PLACEMENT)
+            //{
+            //    m_Instructions.text = "Point device towards ground";
+            //}
+            //else if (PlaneManager.planeMode == PlaneManager.PlaneMode.MIDAIR)
+            //{
+            //    m_Instructions.text = "Tap to place Drone";
+            //}
+            //else if (PlaneManager.planeMode == PlaneManager.PlaneMode.MIDAIR2){
+            //    m_Instructions.text = "Tap to place Spacecraft";
+            //}
         }
     }
 
@@ -141,52 +142,52 @@ public class MidAirUI : MonoBehaviour
     {
         //m_ResetButton.interactable = m_MidAirToggle.interactable = false;
         //m_ResetButton.interactable = m_MidAirToggle2.interactable = false;
+        
 
 
-
-        m_PlacementToggle.isOn = true;
+        //m_PlacementToggle.isOn = true;
     }
 
-    public void UpdateTitle()
-    {
-        switch (PlaneManager.planeMode)
-        {
-            case PlaneManager.PlaneMode.GROUND:
-                m_Title.text = TITLE_GROUNDPLANE;
-                break;
-            case PlaneManager.PlaneMode.MIDAIR:
-                m_Title.text = TITLE_MIDAIR;
-                break;
-            case PlaneManager.PlaneMode.PLACEMENT:
-                m_Title.text = TITLE_PLACEMENT;
-                break;
-            case PlaneManager.PlaneMode.MIDAIR2:
-                m_Title.text = TITLE_MIDAIR2;
-                break;
-        }
-    }
+    //public void UpdateTitle()
+    //{
+    //    switch (PlaneManager.planeMode)
+    //    {
+    //        case PlaneManager.PlaneMode.GROUND:
+    //            m_Title.text = TITLE_GROUNDPLANE;
+    //            break;
+    //        case PlaneManager.PlaneMode.MIDAIR:
+    //            m_Title.text = TITLE_MIDAIR;
+    //            break;
+    //        case PlaneManager.PlaneMode.PLACEMENT:
+    //            m_Title.text = TITLE_PLACEMENT;
+    //            break;
+    //        case PlaneManager.PlaneMode.MIDAIR2:
+    //            m_Title.text = TITLE_MIDAIR2;
+    //            break;
+    //    }
+    //}
 
-    public bool InitializeUI()
-    {
-        // Runs only once after first successful Automatic hit test
-        m_PlacementToggle.interactable = true;
-        m_GroundToggle.interactable = true;
+    //public bool InitializeUI()
+    //{
+    //    // Runs only once after first successful Automatic hit test
+    //    //m_PlacementToggle.interactable = true;
+    //    //m_GroundToggle.interactable = true;
 
-        //if (Vuforia.VuforiaRuntimeUtilities.IsPlayMode())
-        //{
-        //    m_MidAirToggle.interactable = true;
-        //    m_MidAirToggle2.interactable = true;
-        //    m_ResetButton.interactable = true;
-        //}
-        m_MidAirToggle.interactable = true;
-        m_MidAirToggle2.interactable = true;
-        m_ResetButton.interactable = true;
+    //    //if (Vuforia.VuforiaRuntimeUtilities.IsPlayMode())
+    //    //{
+    //    //    m_MidAirToggle.interactable = true;
+    //    //    m_MidAirToggle2.interactable = true;
+    //    //    m_ResetButton.interactable = true;
+    //    //}
+    //    m_MidAirToggle.interactable = true;
+    //    m_MidAirToggle2.interactable = true;
+    //    m_ResetButton.interactable = true;
 
-        // Make the PlacementToggle active
-        m_PlacementToggle.isOn = true;
+    //    // Make the PlacementToggle active
+    //    m_PlacementToggle.isOn = true;
 
-        return true;
-    }
+    //    return true;
+    //}
 
     public bool IsCanvasButtonPressed()
     {
@@ -221,7 +222,7 @@ public class MidAirUI : MonoBehaviour
         {
             case Vuforia.TrackableBehaviour.StatusInfo.INITIALIZING:
 
-                if (Vuforia.VuforiaRuntimeUtilities.GetActiveFusionProvider() ==
+                if (Vuforia.VuforiaRuntimeUtilities.GetActiveFusionProvider() == 
                     Vuforia.FusionProviderType.PLATFORM_SENSOR_FUSION)
                 {
                     m_TrackerStatus.text = "Initializing Tracker";
@@ -230,7 +231,7 @@ public class MidAirUI : MonoBehaviour
                 {
                     m_TrackerStatus.text = "Waiting for anchor to be placed to initialize";
                 }
-
+                
                 break;
             case Vuforia.TrackableBehaviour.StatusInfo.EXCESSIVE_MOTION:
                 m_TrackerStatus.text = "Move slower";
